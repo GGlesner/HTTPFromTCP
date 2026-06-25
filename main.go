70723410cb7a5fb791e8514577ad3141ddb02e32
gglesner <gabriel.glesner@pm.me>
2026-06-25 12:29:34 +0200

    CH1L4

--- main.go
+++ main.go
@@ -4,16 +4,25 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
+)
+
+const (
+	filePath   = "./messages.txt"
+	bufferSize = 8
 )
 
 func main() {
-	const filePath = "messages.txt"
 	fileHandle, err := os.Open(filePath)
 	if err != nil {
 		fmt.Printf("error openning file: %s. error: %s\n", filePath, err.Error())
 	}
 	defer fileHandle.Close()
-	buffer := make([]byte, 8)
+	buffer := make([]byte, bufferSize)
+	line := ""
+	writeOuput := func(s string) {
+		fmt.Printf("read: %s\n", s)
+	}
 	for {
 		n, err := fileHandle.Read(buffer)
 		if err != nil {
@@ -22,6 +31,13 @@ func main() {
 			}
 			fmt.Printf("error reading file: %s. error: %s\n", filePath, err.Error())
 		}
-		fmt.Printf("read: %s\n", string(buffer[:n]))
+		parts := strings.Split(string(buffer[:n]), "\n")
+		m := len(parts)
+		line += parts[0]
+		parts[0] = line
+		for i := 1; i < m; i++ {
+			writeOuput(parts[i-1])
+		}
+		line = parts[m-1]
 	}
 }
